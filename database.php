3edf944b84ce2069e4e2fdcb59ed8f403411c2de
Nishanth C <nishanth.c@s.amity.edu>
2025-07-25 22:28:55 +0530

    commit

--- database.php
+++ database.php
@@ -1,4 +1,8 @@
 <?php
-$conn = mysqli_connect('localhost','root','','bio_lab_management');
-$connn = mysqli_connect('localhost','root','','test');
+$servername = "bio-lab-management.infinityfree.me"; // Or whatever host they provide
+$username = "if0_39558955";
+$password = "MTultnaZeoArHhc";
+$dbname = "if0_39558955_bio_lab_management";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$connn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
